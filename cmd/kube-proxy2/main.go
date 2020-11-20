a74cf765eca6e3bc4202d5bfdc3429740288b514
Mikaël Cluseau <mikael.cluseau@gmail.com>
2020-11-20 10:41:42 +0100

    clean test code

--- cmd/kube-proxy2/main.go
+++ cmd/kube-proxy2/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"os"
 	"runtime/pprof"
@@ -10,14 +9,9 @@ import (
 
 	"k8s.io/klog"
 
-	"github.com/mcluseau/kube-proxy2/pkg/api/localnetv1"
 	"github.com/mcluseau/kube-proxy2/pkg/proxy"
 	"github.com/mcluseau/kube-proxy2/pkg/server"
 	srvendpoints "github.com/mcluseau/kube-proxy2/pkg/server/endpoints"
-)
-
-const (
-	testGRPC = false
 )
 
 var (
@@ -71,46 +65,6 @@ func run(_ *cobra.Command, _ []string) {
 		go klog.Fatal(srv.GRPC.Serve(lis))
 	}
 
-	if testGRPC {
-		doTestGRPC(srv)
-	}
-
 	// wait and exit
 	_, _ = <-srv.QuitCh
-}
-
-func doTestGRPC(srv *proxy.Server) {
-	conn, err := srv.InProcessClient(func(err error) { klog.Error("serve failed: ", err) })
-	if err != nil {
-		klog.Error("error setting up in-memory gRPC: ", err)
-		os.Exit(1)
-	}
-
-	ctx, ctxCancel := context.WithCancel(context.Background())
-	defer func() {
-		ctxCancel()
-		conn.Close()
-
-	}()
-
-	// draft of client run
-	epc := localnetv1.NewEndpointsClient(conn)
-
-	klog.Info("next: calling...")
-	next, err := epc.Next(ctx, &localnetv1.NextFilter{})
-	if err != nil {
-		klog.Info("next failed: ", err)
-		return
-	}
-
-	klog.Info("next: success")
-
-	for {
-		nextItem, err := next.Recv()
-		if err != nil {
-			klog.Info("next: error: ", err)
-			break
-		}
-		klog.Info("next: - item: ", nextItem)
-	}
 }
