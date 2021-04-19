35e1c55a94897cf6155947795a584751d067a85e
Jay Vyas (jayunit100) <jvyas@vmware.com>
2021-04-19 01:07:05 +0000

    update hack/ scripts to use --server

--- hack/kpng-local-up.sh
+++ hack/kpng-local-up.sh
@@ -3,6 +3,20 @@
 
 IMAGE="jayunit100/kpng-server:latest"
 
+function setup_k8s {
+	# make a gopath if not one existing...
+	if "$GOPATH" == "" ; then
+		mkdir -p $HOME/go/
+		export GOPATH=$HOME/go
+		# need kind 0.11 bc 0.10 has a bug w/ kubeproxy=none
+		GO111MODULE="on" go get sigs.k8s.io/kind@master
+		chmod 755 $GOPATH/bin/kind
+		cp $GOPATH/bin/kind /usr/local/bin/kind
+	fi
+	kind version
+	kind create cluster --config kind.yaml
+}
+
 function build {
 	cd ../
 
@@ -20,5 +34,6 @@ function install {
 }
 
 # Comment out build if you just want to install the default, i.e. for quickly getting up and running.
-build
+#setup_k8s
+# build
 install
