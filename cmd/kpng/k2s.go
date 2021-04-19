22f002412887101fef9c008a406cba9620ab9beb
Mikaël Cluseau <mikael.cluseau@gmail.com>
2021-04-19 12:56:35 +0200

    remove the forbidden word

--- cmd/kpng/k2s.go
+++ cmd/kpng/k2s.go
@@ -18,7 +18,7 @@ import (
 // FIXME separate package
 var (
 	kubeConfig string
-	kubeMaster string
+	kubeServer string
 	k2sCfg     = &kube2store.Config{}
 )
 
@@ -31,7 +31,7 @@ func kube2storeCmd() *cobra.Command {
 
 	flags := k2sCmd.PersistentFlags()
 	flags.StringVar(&kubeConfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster. Defaults to envvar KUBECONFIG.")
-	flags.StringVar(&kubeMaster, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+	flags.StringVar(&kubeServer, "server", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 
 	k2sCfg.BindFlags(k2sCmd.PersistentFlags())
 	k2sCmd.AddCommand(storecmds.Commands(setupKube2store)...)
@@ -47,7 +47,7 @@ func setupKube2store() (ctx context.Context, store *proxystore.Store, err error)
 		kubeConfig = os.Getenv("KUBECONFIG")
 	}
 
-	cfg, err := clientcmd.BuildConfigFromFlags(kubeMaster, kubeConfig)
+	cfg, err := clientcmd.BuildConfigFromFlags(kubeServer, kubeConfig)
 	if err != nil {
 		err = fmt.Errorf("Error building kubeconfig: %w", err)
 		return
