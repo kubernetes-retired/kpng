f3d1c4ad01953b95df81fcd4d19668d6f26c37f0
Mikaël Cluseau <mikael.cluseau@gmail.com>
2021-04-29 21:18:50 +0200

    hack: final fixes?

--- hack/kpng-local-up.sh
+++ hack/kpng-local-up.sh
@@ -4,39 +4,42 @@
 IMAGE="jayunit100/kpng-server:latest"
 
 function setup_k8s {
-	# make a gopath if not one existing...
-	if "$GOPATH" == "" ; then
-		mkdir -p $HOME/go/
-		export GOPATH=$HOME/go
-		# need kind 0.11 bc 0.10 has a bug w/ kubeproxy=none
-		GO111MODULE="on" go get sigs.k8s.io/kind@master
-		chmod 755 $GOPATH/bin/kind
-		cp $GOPATH/bin/kind /usr/local/bin/kind
-	fi
-	kind version
+    # make a gopath if not one existing...
+    if "$GOPATH" == "" ; then
+        mkdir -p $HOME/go/
+        export GOPATH=$HOME/go
+        # need kind 0.11 bc 0.10 has a bug w/ kubeproxy=none
+        GO111MODULE="on" go get sigs.k8s.io/kind@master
+        chmod 755 $GOPATH/bin/kind
+        cp $GOPATH/bin/kind /usr/local/bin/kind
+    fi
+    kind version
 
-	echo "****************************************************"
-	kind delete cluster --name kpng-proxy
-	kind create cluster --config kind.yaml
-	echo "****************************************************"
+    echo "****************************************************"
+    kind delete cluster --name kpng-proxy
+    kind create cluster --config kind.yaml
+    echo "****************************************************"
 }
 
 function build {
-	cd ../
+    cd ../
 
-	docker build -t $IMAGE ./
-	docker push $IMAGE
+    docker build -t $IMAGE ./
+    docker push $IMAGE
 
-	cd hack/
+    cd hack/
 }
 
 function install {
-	# substitute it with your changes...
-	cat kpng-deployment-ds.yaml.tmpl | sed "s,KPNG_IMAGE,$IMAGE," > kpng-deployment-ds.yaml
-	kubectl -n kube-system create sa kube-proxy
+    # substitute it with your changes...
+    cat kpng-deployment-ds.yaml.tmpl | sed "s,KPNG_IMAGE,$IMAGE," > kpng-deployment-ds.yaml
+
+    kubectl -n kube-system create sa kube-proxy
     kubectl create clusterrolebinding kube-proxy --clusterrole=system:node-proxier --serviceaccount=kube-system:kube-proxy
-	kubectl delete -f kpng-deployment-ds.yaml
-	kubectl create -f kpng-deployment-ds.yaml
+    kubectl -n kube-system create cm kube-proxy --from-file kubeconfig.conf
+
+    kubectl delete -f kpng-deployment-ds.yaml
+    kubectl create -f kpng-deployment-ds.yaml
 }
 
 # Comment out build if you just want to install the default, i.e. for quickly getting up and running.
