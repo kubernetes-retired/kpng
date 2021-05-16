076d80e25ba1082ffc876e96b3a776caeae1b1da
Ricardo Pchevuzinske Katz <ricardo.katz@gmail.com>
2021-05-16 12:06:11 -0300

    Move ipvs impl to main repo
    Signed-off-by: Ricardo Pchevuzinske Katz <ricardo.katz@gmail.com>

---
+++ cmd/kpng-ipvs-backend/kpng-ipvs-backend.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"k8s.io/klog"
+	"sigs.k8s.io/kpng/backends/ipvs"
+	"sigs.k8s.io/kpng/client"
+)
+
+func main() {
+	err := ipvs.PreRun()
+	if err != nil {
+		klog.Fatalf("Error starting kpng ipvs: %s", err)
+	}
+	client.RunCh(ipvs.Callback, ipvs.BindFlags)
+}
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
+	github.com/vishvananda/netlink v1.1.0 // indirect
 	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
 	golang.org/x/oauth2 v0.0.0-20210427180440-81ed05c6b58c // indirect
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
