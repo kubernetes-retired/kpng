844c27e43f810536a58750383e78af3b352ba559
Ricardo Pchevuzinske Katz <ricardo.katz@gmail.com>
2021-05-16 12:06:11 -0300

    Move ipvs impl to main repo
    Signed-off-by: Ricardo Pchevuzinske Katz <ricardo.katz@gmail.com>

---
+++ backends/ipvs/ipvs.go
@@ -0,0 +1,185 @@
+package ipvs
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+	"strings"
+
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
+	"k8s.io/klog"
+
+	"sigs.k8s.io/kpng/client"
+	"sigs.k8s.io/kpng/pkg/api/localnetv1"
+)
+
+const (
+	// TODO: This is configurable :)
+	DefaultAlgo   = "rr"
+	DefaultWeight = 1
+)
+
+var (
+	clusterIPs map[string]interface{}
+)
+
+func Callback(ch <-chan *client.ServiceEndpoints) {
+
+	var ipvsCfg strings.Builder
+	var err error
+	clusterIPs = make(map[string]interface{})
+
+	for serviceEndpoints := range ch {
+
+		svc := serviceEndpoints.Service
+		endpoints := serviceEndpoints.Endpoints
+
+		//var svcRule strings.Builder
+		// If this is a ClusterIP non headless
+		if (svc.Type == "ClusterIP" && svc.GetIPs().ClusterIP != "None") || svc.Type == "NodePort" || svc.Type == "LoadBalancer" {
+			// TODO: Verify if the IP Address exists on the dummy interface, otherwise it need to be
+			// added
+			if svc.GetIPs().ClusterIP != "" {
+				clusterIPs[svc.GetIPs().ClusterIP] = nil
+			}
+
+			// Future art: build some tree as Mikael did in nft, and verify if and where are the differences
+			// to remove unused ClusterIP Addresses
+			var nodePort bool
+			if svc.Type == "NodePort" || svc.Type == "LoadBalancer" {
+				nodePort = true
+			}
+			cip, err := buildClusterIP(svc, endpoints, nodePort)
+			if err != nil {
+				klog.Warningf("problem creating the service: %s", err)
+			}
+			ipvsCfg.WriteString(cip)
+		}
+	}
+	fmt.Printf("%s", ipvsCfg.String())
+
+	if OnlyOutput != nil && !*OnlyOutput {
+		fmt.Println("Running clear")
+		ipvsClear := exec.Command(*IPVSAdmPath, "--clear")
+
+		err = ipvsClear.Run()
+		if err != nil {
+			klog.Errorf("failed to clear ipvs table: %s", err)
+		}
+
+		fmt.Println("Running restore")
+		ipvsRestore := exec.Command(*IPVSAdmPath, "--restore")
+		ipvsRestore.Stdin = strings.NewReader(ipvsCfg.String())
+
+		err = ipvsRestore.Run()
+		if err != nil {
+			klog.Errorf("failed to execute ipvsadm restore: %s", err)
+		}
+	}
+	ipvsCfg.Reset()
+
+	dummyIface, err := net.InterfaceByName("kube-ipvs0")
+	if err != nil {
+		klog.Errorf("failed to get dummy interface: %s", err)
+	}
+	addrs, err := dummyIface.Addrs()
+	if err != nil {
+		klog.Errorf("failed to get dummy interface addresses: %s", err)
+	}
+	for _, v := range addrs {
+		addr, _, err := net.ParseCIDR(v.String())
+		if addr == nil {
+			klog.Errorf("error parse ip address: %s", v.String())
+			continue
+		}
+		if err != nil {
+			klog.Errorf("error parse ip address: %s - %s", v.String(), err)
+			continue
+		}
+
+		// SKIP IPv
+		if addr.IsLinkLocalUnicast() {
+			continue
+		}
+		if _, ok := clusterIPs[v.String()]; !ok {
+			// Delete the interface address
+			if err := netlink.AddrDel(
+				&netlink.Dummy{
+					LinkAttrs: netlink.LinkAttrs{Name: "kube-ipvs0"},
+				},
+				&netlink.Addr{IPNet: netlink.NewIPNet(addr)}); err != nil {
+				if err != unix.ENXIO {
+					klog.Errorf("error unbind address: %s from interface: %s, err: %v", addr, "kube-ipvs0", err)
+				}
+			}
+		}
+	}
+	for clusteraddr, _ := range clusterIPs {
+		addr := net.ParseIP(clusteraddr)
+		if addr == nil {
+			klog.Errorf("error parse ip address: %s", addr)
+			continue
+		}
+		if err := netlink.AddrAdd(&netlink.Dummy{
+			LinkAttrs: netlink.LinkAttrs{Name: "kube-ipvs0"},
+		},
+			&netlink.Addr{IPNet: netlink.NewIPNet(addr)}); err != nil {
+			// "EEXIST" will be returned if the address is already bound to device
+			if err == unix.EEXIST {
+				continue
+			}
+			klog.Errorf("error bind address: %s to interface: %s, err: %v", clusteraddr, "kube-ipvs0", err)
+		}
+
+	}
+
+}
+
+func buildClusterIP(svc *localnetv1.Service, eps []*localnetv1.Endpoint, nodePort bool) (string, error) {
+	var svcString strings.Builder
+	for _, port := range svc.Ports {
+		var proto string
+		switch port.Protocol {
+		case localnetv1.Protocol_TCP:
+			proto = "-t"
+		case localnetv1.Protocol_SCTP:
+			proto = "--sctp-service "
+		case localnetv1.Protocol_UDP:
+			proto = "-u"
+		default:
+			return "", fmt.Errorf("service %s/%s uses an unknown protocol", svc.Namespace, svc.Name)
+		}
+		tgtPort := port.GetTargetPort()
+		if tgtPort < 1 {
+			tgtPort = port.GetPort()
+		}
+		ipPortAlgo := fmt.Sprintf("-A %s %s:%d -s %s\n", proto, svc.GetIPs().ClusterIP, tgtPort, DefaultAlgo)
+		svcString.WriteString(ipPortAlgo)
+
+		endpoints := buildEndponts(svc.GetIPs().ClusterIP, proto, tgtPort, port.GetPort(), eps)
+		svcString.WriteString(endpoints)
+
+		if nodePort {
+			for _, address := range *NodeAddress {
+				ipPortAlgo := fmt.Sprintf("-A %s %s:%d -s %s\n", proto, address, port.GetNodePort(), DefaultAlgo)
+				svcString.WriteString(ipPortAlgo)
+
+				endpoints := buildEndponts(address, proto, port.GetNodePort(), port.GetPort(), eps)
+				svcString.WriteString(endpoints)
+
+			}
+		}
+	}
+	return svcString.String(), nil
+}
+
+func buildEndponts(VirtualIP, proto string, tgtPort, port int32, endpoints []*localnetv1.Endpoint) string {
+	var strBuilder strings.Builder
+	for _, endpoint := range endpoints {
+		ip := endpoint.GetIPs().V4[0] //TODO:  This is what we call a Gambiarra :D Need to deal better with this thing.
+		ep := fmt.Sprintf("-a %s %s:%d -r %s:%d -m -w %d\n", proto, VirtualIP, tgtPort, ip, port, DefaultWeight)
+		strBuilder.WriteString(ep)
+	}
+	return strBuilder.String()
+}
