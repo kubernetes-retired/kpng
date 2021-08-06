99a80a46c09bf79a6083bf1490e764c590bc1284
Hanamantagoud <hanamantagoud.v.kandagal@est.tech>
2021-08-06 16:29:33 +0530

    ClusterIP and NodePort service IPSet entries added

---
+++ backends/ipvs-as-sink/clusteripsvc.go
@@ -0,0 +1,167 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ipvssink
+
+import (
+	"bytes"
+
+	"sigs.k8s.io/kpng/pkg/api/localnetv1"
+)
+
+func (s *Backend) handleClusterIPService(svc *localnetv1.Service, op Operation) {
+	key := svc.Namespace + "/" + svc.Name
+
+	if op == AddService {
+		isNewService := true
+		if _, ok := s.svcs[key]; ok {
+			isNewService = false
+		}
+
+		if isNewService {
+			s.handleNewClusterIPService(key, svc)
+		} else {
+			s.handleUpdatedClusterIPService(key, svc)
+		}
+	}
+
+	if op == DeleteService {
+		s.AddOrDelClusterIPInIPSet(svc, svc.Ports, DeleteService)
+	}
+}
+
+func (s *Backend) handleNewClusterIPService(key string, svc *localnetv1.Service) {
+	s.svcs[key] = svc
+
+	//Cluster service IP is added to kube-ipvs0 interface
+	s.addServiceIPToKubeIPVSIntf(nil, svc)
+
+	//Cluster service IP is stored in LB tree
+	s.storeLBSvc(svc.Ports, svc.IPs.All().All(), key, ClusterIPService)
+
+	//Cluster service IP needs to be programmed in ipset.
+	s.AddOrDelClusterIPInIPSet(svc, svc.Ports, AddService)
+}
+
+func (s *Backend) handleUpdatedClusterIPService(key string, svc *localnetv1.Service) {
+	// update the svc
+	prevSvc := s.svcs[key]
+	s.svcs[key] = svc
+
+	//Updated Cluster service IP is added/removed from kube-ipvs0 interface
+	s.addServiceIPToKubeIPVSIntf(prevSvc, svc)
+
+	// When existing service gets updated with new port/protocol, endpoints
+	// behind it also needs to be updated into tree, so that they are handled in sync().
+	addedPorts, removedPorts := diffInPortMapping(prevSvc, svc)
+
+	if len(addedPorts) > 0 {
+		//Update the service with added ports into LB tree
+		s.storeLBSvc(addedPorts, svc.IPs.All().All(), key, ClusterIPService)
+		var endPointList []string
+
+		for _, epKV := range s.endpoints.GetByPrefix([]byte(key + "/")) {
+			epIP := epKV.Value.(string)
+			endPointList = append(endPointList, epIP)
+			svcIP := getServiceIP(epIP, svc)
+
+			for _, port := range addedPorts {
+				lbKey := key + "/" + svcIP + "/" + epPortSuffix(port)
+				ipvslb := s.lbs.GetByPrefix([]byte(lbKey))
+
+				s.dests.Set([]byte(lbKey+"/"+epIP), 0, ipvsSvcDst{
+					Svc: ipvslb[0].Value.(ipvsLB).ToService(),
+					Dst: ipvsDestination(epIP, port, s.weight),
+				})
+			}
+		}
+
+		//Cluster service IP needs to be programmed in ipset with added ports.
+		s.AddOrDelClusterIPInIPSet(svc, addedPorts, AddService)
+
+		s.AddOrDelEndPointInIPSet(endPointList, addedPorts, AddEndPoint)
+	}
+
+	// When existing service gets updated with deletion of port/protocol,
+	// endpoint behind it needs to be removed from tree.
+	if len(removedPorts) > 0 {
+		s.deleteLBSvc(removedPorts, svc.IPs.All().All(), key)
+		var endPointList []string
+
+		for _, epKV := range s.endpoints.GetByPrefix([]byte(key + "/")) {
+			epIP := epKV.Value.(string)
+			endPointList = append(endPointList, epIP)
+			svcIP := getServiceIP(epIP, svc)
+
+			for _, port := range removedPorts {
+				lbKey := key + "/" + svcIP + "/" + epPortSuffix(port)
+				s.dests.Delete([]byte(lbKey + "/" + epIP))
+			}
+		}
+
+		s.AddOrDelClusterIPInIPSet(svc, removedPorts, DeleteService)
+
+		s.AddOrDelEndPointInIPSet(endPointList, removedPorts, DeleteEndPoint)
+	}
+}
+
+func (s *Backend) SetEndPointForClusterIPSvc(svcKey, key string, endpoint *localnetv1.Endpoint) {
+	prefix := svcKey + "/" + key + "/"
+	service := s.svcs[svcKey]
+	portList := service.Ports
+
+	for _, endPointIP := range endpoint.IPs.All() {
+		s.endpoints.Set([]byte(prefix+endPointIP), 0, endPointIP)
+		svcIP := getServiceIP(endPointIP, service)
+
+		// add a destination for every LB of this service
+		for _, lbKV := range s.lbs.GetByPrefix([]byte(svcKey + "/" + svcIP)) {
+			lb := lbKV.Value.(ipvsLB)
+			destination := ipvsSvcDst{
+				Svc:             lb.ToService(),
+				Dst:             ipvsDestination(endPointIP, lb.Port, s.weight),
+				isLocalEndPoint: endpoint.Local,
+			}
+			s.dests.Set([]byte(string(lbKV.Key)+"/"+endPointIP), 0, destination)
+		}
+	}
+
+	s.AddOrDelEndPointInIPSet(endpoint.IPs.All(), portList, AddEndPoint)
+}
+
+func (s *Backend) DeleteEndPointForClusterIPSvc(svcKey, key string) {
+	prefix := []byte(svcKey + "/" + key + "/")
+	service := s.svcs[svcKey]
+	portList := service.Ports
+	var endPointList []string
+
+	for _, kv := range s.endpoints.GetByPrefix(prefix) {
+		endPointIP := kv.Value.(string)
+		suffix := []byte("/" + endPointIP)
+
+		for _, destKV := range s.dests.GetByPrefix([]byte(svcKey)) {
+			if bytes.HasSuffix(destKV.Key, suffix) {
+				s.dests.Delete(destKV.Key)
+			}
+		}
+		endPointList = append(endPointList, endPointIP)
+	}
+
+	// remove this endpoint from the endpoints
+	s.endpoints.DeleteByPrefix(prefix)
+
+	s.AddOrDelEndPointInIPSet(endPointList, portList, DeleteEndPoint)
+}
---
+++ backends/ipvs-as-sink/common.go
@@ -0,0 +1,196 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ipvssink
+
+import (
+	"net"
+	"strings"
+
+	"github.com/vishvananda/netlink"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/klog"
+	netutils "k8s.io/utils/net"
+	utilipset "sigs.k8s.io/kpng/backends/util/ipvs"
+	"sigs.k8s.io/kpng/pkg/api/localnetv1"
+)
+
+func (s *Backend) AddOrDelEndPointInIPSet(endPointList []string, portList []*localnetv1.PortMapping, op Operation) {
+	//if !destination.isLocalEndPoint {
+	//	return
+	//}
+	for _, port := range portList {
+		for _, endPointIP := range endPointList {
+			epIPFamily := getIPFamily(endPointIP)
+			ipSetName := loopBackIPSetMap[epIPFamily]
+			entry := getEndPointEntry(endPointIP, port)
+			if valid := s.ipsetList[ipSetName].validateEntry(entry); !valid {
+				klog.Errorf("error adding entry to ipset. entry:%s, ipset:%s", entry.String(), s.ipsetList[ipSetName].Name)
+				return
+			}
+			if op == AddEndPoint {
+				s.ipsetList[ipSetName].newEntries.Insert(entry.String())
+			}
+			if op == DeleteEndPoint {
+				s.ipsetList[ipSetName].deleteEntries.Insert(entry.String())
+			}
+		}
+	}
+}
+
+func getIPFamily(ipAddr string) v1.IPFamily {
+	var ipAddrFamily v1.IPFamily
+	if netutils.IsIPv4String(ipAddr) {
+		ipAddrFamily = v1.IPv4Protocol
+	}
+
+	if netutils.IsIPv6String(ipAddr) {
+		ipAddrFamily = v1.IPv6Protocol
+	}
+	return ipAddrFamily
+}
+
+func getEndPointEntry(endPointIP string, port *localnetv1.PortMapping) *utilipset.Entry {
+	return &utilipset.Entry{
+		IP:       endPointIP,
+		Port:     int(port.TargetPort),
+		Protocol: strings.ToLower(port.Protocol.String()),
+		IP2:      endPointIP,
+		SetType:  utilipset.HashIPPortIP,
+	}
+}
+
+func (s *Backend) AddOrDelClusterIPInIPSet(svc *localnetv1.Service, portList []*localnetv1.PortMapping, op Operation) {
+	svcIPFamily := getServiceIPFamily(svc)
+
+	for _, port := range portList {
+		for _, ipFamily := range svcIPFamily {
+			var clusterIP string
+			if ipFamily == v1.IPv4Protocol {
+				clusterIP = svc.IPs.ClusterIPs.V4[0]
+			}
+			if ipFamily == v1.IPv6Protocol {
+				clusterIP = svc.IPs.ClusterIPs.V6[0]
+			}
+			ipSetName := clusterIPSetMap[ipFamily]
+			// Capture the clusterIP.
+			entry := getIPSetEntry(clusterIP, port)
+			// add service Cluster IP:Port to kubeServiceAccess ip set for the purpose of solving hairpin.
+			if valid := s.ipsetList[ipSetName].validateEntry(entry); !valid {
+				klog.Errorf("error adding entry :%s, to ipset:%s", entry.String(), s.ipsetList[ipSetName].Name)
+				return
+			}
+			if op == AddService {
+				s.ipsetList[ipSetName].newEntries.Insert(entry.String())
+			}
+			if op == DeleteService {
+				s.ipsetList[ipSetName].deleteEntries.Insert(entry.String())
+			}
+		}
+	}
+}
+
+func getServiceIPFamily(svc *localnetv1.Service) []v1.IPFamily {
+	var svcIPFamily []v1.IPFamily
+	if len(svc.IPs.ClusterIPs.V4) > 0 && len(svc.IPs.ClusterIPs.V6) == 0 {
+		svcIPFamily = append(svcIPFamily, v1.IPv4Protocol)
+	}
+
+	if len(svc.IPs.ClusterIPs.V6) > 0 && len(svc.IPs.ClusterIPs.V4) == 0 {
+		svcIPFamily = append(svcIPFamily, v1.IPv6Protocol)
+	}
+
+	if len(svc.IPs.ClusterIPs.V4) > 0 && len(svc.IPs.ClusterIPs.V6) > 0 {
+		svcIPFamily = append(svcIPFamily, v1.IPv4Protocol, v1.IPv6Protocol)
+	}
+	return svcIPFamily
+}
+
+func getIPSetEntry(svcIP string, port *localnetv1.PortMapping) *utilipset.Entry {
+	return &utilipset.Entry{
+		IP:       svcIP,
+		Port:     int(port.Port),
+		Protocol: strings.ToLower(port.Protocol.String()),
+		SetType:  utilipset.HashIPPort,
+	}
+}
+
+func getServiceIP(endPointIP string, svc *localnetv1.Service) string {
+	var svcIP string
+	if netutils.IsIPv4String(endPointIP) {
+		svcIP = svc.IPs.ClusterIPs.V4[0]
+	}
+	if netutils.IsIPv6String(endPointIP) {
+		svcIP = svc.IPs.ClusterIPs.V6[0]
+	}
+	return svcIP
+}
+
+func (s *Backend) addServiceIPToKubeIPVSIntf(prevSvc, curr *localnetv1.Service) {
+	// sync dummy IPs
+	var prevIPs *localnetv1.IPSet
+	if prevSvc == nil {
+		prevIPs = localnetv1.NewIPSet()
+	} else {
+		prevIPs = prevSvc.IPs.All()
+	}
+
+	currentIPs := curr.IPs.All()
+
+	added, removed := prevIPs.Diff(currentIPs)
+
+	for _, ip := range asDummyIPs(added) {
+		if _, ok := s.dummyIPsRefCounts[ip]; !ok {
+			// IP is not referenced so we must add it
+			klog.V(2).Info("adding dummy IP ", ip)
+
+			_, ipNet, err := net.ParseCIDR(ip)
+			if err != nil {
+				klog.Fatalf("failed to parse ip/net %q: %v", ip, err)
+			}
+
+			if err = netlink.AddrAdd(s.dummy, &netlink.Addr{IPNet: ipNet}); err != nil {
+				klog.Error("failed to add dummy IP ", ip, ": ", err)
+			}
+		}
+
+		s.dummyIPsRefCounts[ip]++
+	}
+
+	for _, ip := range asDummyIPs(removed) {
+		s.dummyIPsRefCounts[ip]--
+	}
+}
+
+func (s *Backend) storeLBSvc(portList []*localnetv1.PortMapping, addrList []string, key, svcType string) {
+	for _, ip := range addrList {
+		prefix := key + "/" + ip + "/"
+		for _, port := range portList {
+			lbKey := prefix + epPortSuffix(port)
+			s.lbs.Set([]byte(lbKey), 0, ipvsLB{IP: ip, ServiceKey: key, Port: port, SchedulingMethod: s.schedulingMethod, ServiceType: svcType})
+		}
+	}
+}
+
+func (s *Backend) deleteLBSvc(portList []*localnetv1.PortMapping, addrList []string, key string) {
+	for _, ip := range addrList {
+		prefix := key + "/" + ip + "/"
+		for _, port := range portList {
+			lbKey := prefix + epPortSuffix(port)
+			s.lbs.Delete([]byte(lbKey))
+		}
+	}
+}
