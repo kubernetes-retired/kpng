0674b508c853a01afe856cba06adaecef4c5f65e
Mikaël Cluseau <mikael.cluseau@gmail.com>
2021-11-09 11:29:50 +0100

    client: serviceevents: wrapper for decoder.Interface

---
+++ client/serviceevents/wrap.go
@@ -0,0 +1,45 @@
+package serviceevents
+
+import (
+	"sigs.k8s.io/kpng/api/localnetv1"
+	"sigs.k8s.io/kpng/client/localsink/decoder"
+)
+
+type wrapper struct {
+	// backend
+	decoder.Interface
+	// listener
+	l *ServicesListener
+}
+
+var _ decoder.Interface = wrapper{}
+
+func Wrap(backend decoder.Interface) decoder.Interface {
+	l := New()
+
+	if v, ok := backend.(PortsListener); ok {
+		l.PortsListener = v
+	}
+	if v, ok := backend.(IPsListener); ok {
+		l.IPsListener = v
+	}
+	if v, ok := backend.(IPPortsListener); ok {
+		l.IPPortsListener = v
+	}
+
+	wrap := wrapper{
+		Interface: backend,
+		l:         l,
+	}
+	return wrap
+}
+
+func (w wrapper) SetService(service *localnetv1.Service) {
+	w.Interface.SetService(service)
+	w.l.SetService(service)
+}
+
+func (w wrapper) DeleteService(namespace, name string) {
+	w.l.DeleteService(namespace, name)
+	w.Interface.DeleteService(namespace, name)
+}
