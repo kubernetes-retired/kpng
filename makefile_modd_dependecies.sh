528d6012b40bb0f8469ff8a191901e999ce5d1bc
emabota <euclidesc@gmail.com>
2023-01-05 19:12:13 +0200

    Added descriptive echo messages

--- makefile_modd_dependecies.sh
+++ makefile_modd_dependecies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "Hello Euclides!"
+echo "Installing Protoc"
 
+# Protoc installation 
 mkdir protoc 
 # Would it be better or safe to work on the /tmp folder instead of protoc?
 cd protoc 
@@ -10,4 +11,9 @@ unzip protoc.zip
 sudo cp bin/protoc /usr/local/bin 
 sudo cp -r include/google /usr/local/include 
 cd ../
-sudo rm -r protoc
+sudo rm -r protoc 
+
+# Libraries 
+echo "Installing libnl-3-dev and libnl-genl-3-dev libraries"
+sudo apt-get install libnl-3-dev #Fixes fatal error: netlink/netlink.h: No such file or directory
+sudo apt-get install libnl-genl-3-dev #Fixes /usr/bin/ld: cannot find -lnl-genl-3
