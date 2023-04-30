d4d4f768eff6f8e666c32bee11b4d27c4b1bd4b2
pa <per@kaloom.com>
2023-04-30 12:17:28 -0400

    one more test run

--- hack/test_skip_list.sh
+++ hack/test_skip_list.sh
@@ -19,38 +19,42 @@
 
 GINKGO_SKIP_ipv4_iptables_TEST="should be updated after adding or deleting ports\
 |should serve multiport endpoints from pods\
-|should check kube-proxy urls\
-|should be rejected when no endpoints exist\
-|should be able to preserve UDP traffic when server pod cycles for a NodePort service\
-|should run through the lifecycle of Pods and PodStatus\
 |should mirror a custom Endpoint with multiple subsets and same IP address\
 |should support a Service with multiple ports specified in multiple EndpointSlices\
-|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices|" 
+|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
+|should check kube-proxy urls\
+|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices"
+
+#|should be rejected when no endpoints exist\
+#|should be able to preserve UDP traffic when server pod cycles for a NodePort service\
+#|should run through the lifecycle of Pods and PodStatus\
 
 
 GINKGO_SKIP_ipv6_iptables_TEST="should be updated after adding or deleting ports\
 |should serve multiport endpoints from pods\
-|should check kube-proxy urls\
-|should be able to change the type from ClusterIP to ExternalName\
-|should create endpoints for unready pods\
-|should be able to change the type from NodePort to ExternalName\
-|should provide DNS for services\
-|should provide DNS for the cluster\
-|should provide DNS for pods for Subdomain\
-|should function for endpoint-Service: udp\
-|should be able to preserve UDP traffic when server pod cycles for a NodePort service\
 |should mirror a custom Endpoint with multiple subsets and same IP address\
 |should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
 |should support a Service with multiple ports specified in multiple EndpointSlices" 
 
+#|should check kube-proxy urls\
+#|should be able to change the type from ClusterIP to ExternalName\
+#|should create endpoints for unready pods\
+#|should be able to change the type from NodePort to ExternalName\
+#|should provide DNS for services\
+#|should provide DNS for the cluster\
+#|should provide DNS for pods for Subdomain\
+#|should function for endpoint-Service: udp\
+#|should be able to preserve UDP traffic when server pod cycles for a NodePort service\
+
 GINKGO_SKIP_dual_iptables_TEST="should be updated after adding or deleting ports\
 |should serve multiport endpoints from pods\
-|should be able to preserve UDP traffic when server pod cycles for a NodePort service\
-|should check kube-proxy urls\
 |should support a Service with multiple ports specified in multiple EndpointSlices\
 |should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
 |should mirror a custom Endpoint with multiple subsets and same IP address" 
-    
+
+#|should be able to preserve UDP traffic when server pod cycles for a NodePort service\
+#|should check kube-proxy urls\
+
 GINKGO_SKIP_ipv4_ipvs_TEST="should check kube-proxy urls\
 |should work after the service has been recreated\
 |should be able to preserve UDP traffic when server pod cycles for a NodePort service\
@@ -70,7 +74,7 @@ GINKGO_SKIP_ipv4_ipvs_TEST="should check kube-proxy urls\
 |should be able to create a functioning NodePort service\
 |should create endpoints for unready pods\
 |should be able to connect to terminating and unready endpoints if PublishNotReadyAddresses is true\
-|should function for multiple endpoint-Services with same selecto"
+|should function for multiple endpoint-Services with same selector"
 
 GINKGO_SKIP_ipv6_ipvs_TEST="should have session affinity work for NodePort service\
 |should have session affinity timeout work for NodePort service\
@@ -124,84 +128,95 @@ GINKGO_SKIP_dual_ipvs_TEST="should work after the service has been recreated\
 |should be able to create a functioning NodePort service\
 |should be able to change the type from ExternalName to NodePort"
 
-GINKGO_SKIP_ipv4_nft_TEST="should check kube-proxy urls\
-|should work with the pod containing more than 6 DNS search paths and longer than 256 search list characters\
-|should run through the lifecycle of Pods and PodStatus\
-|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
-|should mirror a custom Endpoint with multiple subsets and same IP address\
-|should support a Service with multiple ports specified in multiple EndpointSlices"
+GINKGO_SKIP_ipv4_nft_TEST="should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
+|should support a Service with multiple ports specified in multiple EndpointSlices\
+|should check kube-proxy urls\
+|should mirror a custom Endpoint with multiple subsets and same IP address"
 
+#|should work with the pod containing more than 6 DNS search paths and longer than 256 search list characters\
+#|should run through the lifecycle of Pods and PodStatus\
 
 GINKGO_SKIP_ipv6_nft_TEST="should work after the service has been recreated\
-|ServiceAccountIssuerDiscovery should support OIDC discovery of service account issuer\
-|should be able to change the type from ClusterIP to ExternalName\
 |should be able to change the type from ExternalName to ClusterIP\
 |should be able to change the type from ExternalName to NodePort\
-|should be able to change the type from NodePort to ExternalName\
 |should be able to connect to terminating and unready endpoints if PublishNotReadyAddresses is true\
 |should be able to create a functioning NodePort service\
-|should be able to handle large requests: http\
-|should be able to handle large requests: udp\
 |should be able to preserve UDP traffic when server pod cycles for a NodePort service\
 |should be able to switch session affinity for NodePort service\
 |should be able to switch session affinity for service with type clusterIP\
 |should be able to up and down services\
 |should be able to update service type to NodePort listening on same port number but different protocols\
-|should be possible to connect to a service via ExternalIP when the external IP is not assigned to a node\
 |should be updated after adding or deleting port\
 |should check kube-proxy urls\
-|should create endpoints for unready pods\
-|should function for client IP based session affinity: http\
-|should function for client IP based session affinity: udp\
-|should function for endpoint-Service: http\
-|should function for endpoint-Service: udp\
-|should function for multiple endpoint-Services with same selector\
-|should function for node-Service: http\
-|should function for node-Service: udp\
-|should function for pod-Service: http\
-|should function for pod-Service: udp\
-|should function for service endpoints using hostNetwork\
 |should have session affinity timeout work for NodePort service\
+|should be rejected when no endpoints exist\
 |should have session affinity timeout work for service with type clusterIP\
 |should have session affinity work for NodePort service\
 |should have session affinity work for service with type clusterIP\
 |should implement service.kubernetes.io/headless\
 |should implement service.kubernetes.io/service-proxy-name\
-|should provide DNS for pods for Subdomain\
-|should provide DNS for services\
-|should provide DNS for the cluster\
 |should serve multiport endpoints from pods\
-|should support basic nodePort: udp functionality\
-|should update endpoints: http\
-|should update nodePort: udp\
-|should work after the service has been recreated\
-|work after the service has been recreated\
-|should update endpoints: udp\
-|should update nodePort: http\
 |should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
+|should provide DNS for pods for Subdomain\
+|should be possible to connect to a service via ExternalIP when the external IP is not assigned to a node\
 |should support a Service with multiple ports specified in multiple EndpointSlices\
+|should function for multiple endpoint-Services with same selector\
+|should function for service endpoints using hostNetwork\
+|should function for node-Service: udp\
+|should support basic nodePort: udp functionality\
+|should be able to handle large requests: http\
+|should update nodePort: http\
+|should function for client IP based session affinity: http\
 |should mirror a custom Endpoint with multiple subsets and same IP address"
 
-GINKGO_SKIP_dual_nft_TEST="should check kube-proxy urls\
+#|should update endpoints: http\
+#|should update nodePort: udp\
+#|should work after the service has been recreated\
+#|work after the service has been recreated\
+#|should update endpoints: udp\
+#|should provide DNS for services\
+#|should provide DNS for the cluster\
+#|should be able to change the type from NodePort to ExternalName\
+#|ServiceAccountIssuerDiscovery should support OIDC discovery of service account issuer\
+#|should be able to change the type from ClusterIP to ExternalName\
+#|should be able to handle large requests: udp\
+#|should create endpoints for unready pods\
+#|should function for client IP based session affinity: udp\
+#|should function for endpoint-Service: http\
+#|should function for endpoint-Service: udp\
+#|should function for node-Service: http\
+#|should function for pod-Service: http\
+#|should function for pod-Service: udp\
+
+GINKGO_SKIP_dual_nft_TEST="should mirror a custom Endpoint with multiple subsets and same IP address\
 |should be rejected when no endpoints exist\
-|should check kube-proxy urls\
-|should be rejected when no endpoints exist\
-|should create a LimitRange with defaults and ensure pod has those defaults applied.\
 |should work with the pod containing more than 6 DNS search paths and longer than 256 search list characters\
 |should support a Service with multiple ports specified in multiple EndpointSlices\
-|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
-|should mirror a custom Endpoint with multiple subsets and same IP address" 
+|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices"
 
-GINKGO_SKIP_ipv4_ebpf_TEST="should serve multiport endpoints from pods\
+#|should check kube-proxy urls\
+#|should be rejected when no endpoints exist\
+#|should create a LimitRange with defaults and ensure pod has those defaults applied.\
+
+
+GINKGO_SKIP_ipv4_ebpf_TEST="should work after the service has been recreated\
+|should serve endpoints on same port and different protocols\
+|should serve a basic endpoint from pods\
+|should preserve source pod IP for traffic thru service cluster IP\
+|should implement service.kubernetes.io/service-proxy-name\
+|should implement service.kubernetes.io/headless\
+|should serve multiport endpoints from pods\
 |should have session affinity work for service with type clusterIP\
 |should have session affinity work for NodePort service\
 |should have session affinity timeout work for service with type clusterIP\
 |should have session affinity timeout work for NodePort service\
+|should create endpoints for unready pods\
 |should be updated after adding or deleting ports\
 |should be rejected when no endpoints exist\
 |should be rejected for evicted pods\
 |should be possible to connect to a service via ExternalIP when the external IP is not assigned to a node\
 |should be able to update service type to NodePort listening on same port number but different protocols\
+|should be able to preserve UDP traffic when server pod cycles for a ClusterIP service\
 |should be able to switch session affinity for service with type clusterIP\
 |should be able to switch session affinity for NodePort service\
 |should be able to create a functioning NodePort service\
@@ -209,71 +224,63 @@ GINKGO_SKIP_ipv4_ebpf_TEST="should serve multiport endpoints from pods\
 |should be able to change the type from ExternalName to NodePort\
 |should be able to change the type from ExternalName to ClusterIP\
 |should allow pods to hairpin back to themselves through services\
-|should drop INVALID conntrack entries\
+|should support a Service with multiple ports specified in multiple EndpointSlices\
 |should be able to preserve UDP traffic when server pod cycles for a NodePort service\
 |should be able to preserve UDP traffic when initial unready endpoints get ready\
-|should work after the service has been recreated\
-|should serve a basic endpoint from pods\
-|should preserve source pod IP for traffic thru service cluster IP\
-|should implement service.kubernetes.io/service-proxy-name\
-|should implement service.kubernetes.io/headless\
-|should create endpoints for unready pods\
 |should be able to up and down services\
-|should be able to preserve UDP traffic when server pod cycles for a ClusterIP service\
-|should update endpoints: http\
-|should support basic nodePort: udp functionality\
-|should update nodePort: udp\
-|should function for node-Service: udp\
+|should mirror a custom Endpoint with multiple subsets and same IP address\
+|should drop INVALID conntrack entries\
 |should create and stop a working application\
 |should be able to change the type from NodePort to ExternalName\
 |should be able to change the type from ClusterIP to ExternalName \
+|should support basic nodePort: udp functionality\
+|should provide DNS for ExternalName services\
 |should resolve DNS of partial qualified names for services\
 |should provide DNS for services\
-|should provide DNS for pods for Subdomain\
-|should provide DNS for ExternalName services\
-|ServiceAccountIssuerDiscovery should support OIDC discovery of service account issuer\
-|Networking Granular Checks: Services [It] should function for node-Service: http\
-|should check kube-proxy urls\
 |should resolve DNS of partial qualified names for services on hostNetwork pods with dnsPolicy: ClusterFirstWithHostNet\
-|should work with the pod containing more than 6 DNS search paths and longer than 256 search list characters\
 |should resolve DNS of partial qualified names for the cluster\
-|should provide DNS for the cluster\
-|should function for service endpoints using hostNetwork\
-|should be able to handle large requests: udp\
-|should update endpoints: udp\
-|should function for pod-Service: udp\
-|should function for pod-Service: http\
-|should function for client IP based session affinity: http\
-|should function for multiple endpoint-Services with same selector\
+|should function for node-Service: udp\
+|ServiceAccountIssuerDiscovery should support OIDC discovery of service account issuer\
 |should function for node-Service: http\
-|should function for endpoint-Service: http\
-|should function for pod-Service: udp\
-|should function for client IP based session affinity: udp\
+|should function for client IP based session affinity: http\
 |should function for endpoint-Service: udp\
-|should run through the lifecycle of Pods and PodStatus\
-|should update nodePort: http\
-|should be able to handle large requests: http\
-|should mirror a custom Endpoint with multiple subsets and same IP address\
-|should support a Service with multiple ports specified in multiple EndpointSlices\
-|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
-|should serve endpoints on same port and different protocols"
+|should work with the pod containing more than 6 DNS search paths and longer than 256 search list characters\
+|should provide DNS for the cluster\
+|should provide DNS for pods for Subdomain\
+|should support a Service with multiple endpoint IPs specified in multiple EndpointSlices"
+
+#|should update endpoints: http\
+#|should update nodePort: udp\
+#|Networking Granular Checks: Services [It] should function for node-Service: http\
+#|should check kube-proxy urls\
+#|should function for service endpoints using hostNetwork\
+#|should be able to handle large requests: udp\
+#|should update endpoints: udp\
+#|should function for pod-Service: udp\
+#|should function for pod-Service: http\
+#|should function for multiple endpoint-Services with same selector\
+#|should function for endpoint-Service: http\
+#|should function for pod-Service: udp\
+#|should function for client IP based session affinity: udp\
+#|should run through the lifecycle of Pods and PodStatus\
+#|should update nodePort: http\
+#|should be able to handle large requests: http\
+
 
 GINKGO_SKIP_ipv4_userspacelin_TEST="should preserve source pod IP for traffic thru service cluster IP\
-|should be rejected when no endpoints exist\
 |should be rejected for evicted pods\
 |should be able to switch session affinity for service with type clusterIP\
 |should be able to switch session affinity for NodePort service\
-|should be able to preserve UDP traffic when initial unready endpoints get ready\
-|should have session affinity timeout work for service with type clusterIP\
-|should be able to preserve UDP traffic when server pod cycles for a ClusterIP service\
 |should be able to preserve UDP traffic when server pod cycles for a NodePort service\
-|should check kube-proxy urls\
-|should preserve source pod IP for traffic thru service cluster IP\
-|should have session affinity timeout work for NodePort service\
-|should run through the lifecycle of Pods and PodStatus\
 |should support a Service with multiple ports specified in multiple EndpointSlices\
 |should support a Service with multiple endpoint IPs specified in multiple EndpointSlices\
-|should mirror a custom Endpoint with multiple subsets and same IP address" 
+|should have session affinity timeout work for service with type clusterIP\
+|should be able to preserve UDP traffic when initial unready endpoints get ready\
+|should check kube-proxy urls\
+|should have session affinity timeout work for NodePort service\
+|should be able to preserve UDP traffic when server pod cycles for a ClusterIP service\
+|should be rejected when no endpoints exist" 
 
-
-
+#|should mirror a custom Endpoint with multiple subsets and same IP address\
+#|should preserve source pod IP for traffic thru service cluster IP\
+#|should run through the lifecycle of Pods and PodStatus\
